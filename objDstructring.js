cfd7b9df0425f9d0344494f464d2ff07711075bf
Masuk Khan <masukkhanemail@gmail.com>
2025-08-02 07:16:30 +0530

    day 5 and learned about object destructuring

---
+++ objDstructring.js
@@ -0,0 +1,14 @@
+const obj = {
+    name: "masuk",
+    age:25,
+    gender: "male",
+    edu : "BTech"
+}
+
+const {name} = obj
+console.log(name)
+
+const {gender:kon} = obj
+console.log(kon)
+
+console.log(obj.name)
