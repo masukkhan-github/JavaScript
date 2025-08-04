9b2bdfd75aae81ed87d784e1808a65b868fa0820
Masuk Khan <masukkhanemail@gmail.com>
2025-08-04 17:59:21 +0530

    6 of learning js with loops

---
+++ loop.js
@@ -0,0 +1,15 @@
+for(let i=0;i<10;i++){
+    console.log(`value of i id ${i}`)
+}
+
+let i =1;
+while(i<10){
+    console.log(`while loop i =${i}`)
+    i++
+}
+
+let j =0;
+do{
+    console.log(`do loop j${j}`)
+    j++
+}while(j<5)
